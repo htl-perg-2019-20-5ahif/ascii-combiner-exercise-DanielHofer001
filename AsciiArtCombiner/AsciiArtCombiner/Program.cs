7473ca934edd4b467545c3bbf7d8dbb0d43ad72b
Daniel Hofer <dh@x-net.at>
2019-10-10 09:48:27 +0200

    working solution

---
+++ AsciiArtCombiner/AsciiArtCombiner/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AsciiArtCombiner
+{
+    class Program
+    {
+        static List<string[]> listOfReadFiles = new List<string[]>();
+        static void Main(string[] args)
+        {
+            if (args.Length > 2 && args[0] == "run")
+            {
+
+                foreach (var file in args.Skip(1).ToArray())
+                {
+                    if (!ReadFile(file)) return;
+                    
+                }
+                foreach (var read in listOfReadFiles)
+                {
+                    printArray(read);
+
+                }
+                Combiner.Combiner c = new Combiner.Combiner();
+                if (c.checkFiles(listOfReadFiles))
+                {
+                    string[] result = listOfReadFiles[0];
+                    for (int i =1; i< listOfReadFiles.Count; i++)
+                    {
+                        result=c.Combine(result, listOfReadFiles[i]);
+                        printArray(result);
+                    }                    
+                }
+                else
+                {
+                    Console.WriteLine("Error in files height/length");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine("Please enter:");
+                Console.WriteLine("Combiner file1.txt file2.txt file3.txt ...");
+            }
+        }
+
+        private static void printArray(string[] v)
+        {
+            foreach (var item in v)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static bool ReadFile(string fileName)
+        {
+            try
+            { 
+                var input = System.IO.File.ReadAllLines(fileName);
+                listOfReadFiles.Add(input);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+
+    }
+}
---
+++ AsciiArtCombiner/Combiner/Combiner.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Combiner
+{
+    public class Combiner
+    {
+
+        public Combiner()
+        {
+
+        }
+        public bool checkFiles(List<string[]> list)
+        {
+            var height = list[0].Length;
+            var length = list[0][0].Length;
+            foreach (var element in list)
+            {
+                if (height != element.Length)
+                {
+                    return false;
+                }
+                foreach (var line in element)
+                {
+                    if (line.Length != length)
+                    {
+                        return false;
+                    }
+                }
+
+
+            }
+            return true;
+        }
+        public string[] Combine(string[] file1, string[] file2)
+        {
+            //result with new height of file1
+            string[] result = new string[file1.Length];
+
+            for (int i = 0; i < file1.Length; i++)
+            {
+                //get the lines
+                var arr = file1[i].ToCharArray();
+                var arr2 = file2[i].ToCharArray();
+
+
+                for (int c = 0; c < arr.Length; c++)
+                {
+                    if (arr[c] == ' ')
+                    {
+                        result[i] += arr2[c];
+                    }
+                    else
+                    {
+                        result[i] += arr[c];
+                    }
+                }
+
+            }
+            return result;
+        }
+    }
+}
